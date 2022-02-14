b5bb35b09f063d5a5c6fe838c071f189e75f926a
justYu2001 <jack4781012@gmail.com>
2022-02-14 23:19:05 +0800

    fix: camera page layout
    Use safearea to wrap the camera page.

--- lib/screens/camera.dart
+++ lib/screens/camera.dart
@@ -61,6 +61,8 @@ class _CameraState extends State<Camera> {
 
   @override
   Widget build(BuildContext context) {
+    bool hasNotch = MediaQuery.of(context).viewPadding.top > 0;
+
     if (_cameraController == null || !_cameraController!.value.isInitialized) {
       return Container(
         decoration: const BoxDecoration(
@@ -71,85 +73,87 @@ class _CameraState extends State<Camera> {
 
     return CameraPreview(
       _cameraController!,
-      child: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(
-              vertical: 18.0,
-              horizontal: 18.0,
+      child: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.symmetric(
+                vertical: hasNotch ? 0.0 : 18.0,
+                horizontal: 18.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const SizedBox(width: 32.0),
+                  _iconButton(Icons.flash_off, null),
+                  _iconButton(Icons.arrow_forward, null),
+                ],
+              ),
             ),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const SizedBox(width: 32.0),
-                _iconButton(Icons.flash_off, null),
-                _iconButton(Icons.arrow_forward, null),
-              ],
-            ),
-          ),
-          Expanded(
-            child: GestureDetector(
-              onScaleUpdate: (scaleStartDetails) {
-                double newZoom = 0.0;
-                if (scaleStartDetails.scale > 1.0) {
-                  newZoom = _zoom + 0.05;
-                } else if (scaleStartDetails.scale < 1.0) {
-                  newZoom = _zoom - 0.05;
-                }
+            Expanded(
+              child: GestureDetector(
+                onScaleUpdate: (scaleStartDetails) {
+                  double newZoom = 0.0;
+                  if (scaleStartDetails.scale > 1.0) {
+                    newZoom = _zoom + 0.05;
+                  } else if (scaleStartDetails.scale < 1.0) {
+                    newZoom = _zoom - 0.05;
+                  }
 
-                if (newZoom >= 1.0 && newZoom <= 9.0) {
-                  _zoom = newZoom;
-                }
+                  if (newZoom >= 1.0 && newZoom <= 9.0) {
+                    _zoom = newZoom;
+                  }
 
-                _cameraController?.setZoomLevel(_zoom);
-              },
+                  _cameraController?.setZoomLevel(_zoom);
+                },
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                SizedBox(
-                  width: 36.0,
-                  height: 36.0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(4.0),
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 3.0,
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: hasNotch ? 0.0 : 24.0,),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  SizedBox(
+                    width: 36.0,
+                    height: 36.0,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4.0),
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 3.0,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Container(
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.transparent,
-                      border: Border.all(
-                        color: Colors.white,
-                        width: 3.0,
-                      ),
-                    ),
-                    padding: const EdgeInsets.all(1.0),
-                    child: ClipOval(
-                      child: Material(
-                        color: Colors.white,
-                        child: InkWell(
-                          splashColor: Colors.grey[300]?.withOpacity(0.7),
-                          splashFactory: InkRipple.splashFactory,
-                          onTap: () {},
-                          child: const SizedBox(width: 56, height: 56),
+                  Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.transparent,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 3.0,
                         ),
                       ),
-                    )),
-                _iconButton(Icons.flip_camera_android, null),
-              ],
+                      padding: const EdgeInsets.all(1.0),
+                      child: ClipOval(
+                        child: Material(
+                          color: Colors.white,
+                          child: InkWell(
+                            splashColor: Colors.grey[300]?.withOpacity(0.7),
+                            splashFactory: InkRipple.splashFactory,
+                            onTap: () {},
+                            child: const SizedBox(width: 56, height: 56),
+                          ),
+                        ),
+                      )),
+                  _iconButton(Icons.flip_camera_android, null),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
